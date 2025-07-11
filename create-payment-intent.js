42f1d9a27e25b60f9796ad79da9b2a868c6ee030
bgmalexk <bgmalexk@gmail.com>
2025-07-11 15:21:03 +0100

    Update create-payment-intent.js

--- create-payment-intent.js
+++ create-payment-intent.js
@@ -1,15 +1,18 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from 'stripe';
 
-module.exports = async (req, res) => {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(405).send({ message: 'Only POST allowed' });
+    res.setHeader('Allow', 'POST');
+    return res.status(405).json({ message: 'Only POST allowed' });
   }
 
   const { amount } = req.body;
 
   try {
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount * 100, // convert pounds to pence
+      amount: amount * 100, // перевод фунтов в пенсы
       currency: 'gbp',
     });
 
@@ -17,4 +20,4 @@ module.exports = async (req, res) => {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-};
+}
